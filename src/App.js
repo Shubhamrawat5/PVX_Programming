3552d1155b22bdb079bbdb81b11eede94316c449
anirudh1713 <anirudhatodaria11@gmail.com>
2021-04-20 18:07:45 +0530

    Add card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Card from './components/Card/Card';
+
 function App() {
   return (
-    <div>
-      app
+    <div className="bg-gray-100 h-screen p-5 w-full">
+      <div className="grid grid-cols-4">
+        <Card />
+      </div>
     </div>
   );
 }
