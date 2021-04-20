0b4517fa2be4ed2e4dcaace1e5dc32937c716d6e
Shubham Rawat <sr856161@gmail.com>
2021-04-20 23:40:41 +0530

    add navbar component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+function Nav() {
+  return (
+    <nav className="text-3xl p-2 text-center font-semibold bg-purple-900 text-white">
+      PVX PROGRAMMING
+    </nav>
+  );
+}
+
+export default Nav;
